830ee3d0d89e5ae921ceca86190c8d54e27c560b
Shah Hafiz <shahhafiz08@gmail.com>
2024-05-14 22:22:58 +0530

    Fix clerk imports

--- app/Api/Webhook/clerk/route.ts
+++ app/Api/Webhook/clerk/route.ts
@@ -1,9 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
+import { CreateUserParams, UpdateUserParams } from '@/types'
  
 export async function POST(req: Request) {
  
@@ -57,12 +57,12 @@ export async function POST(req: Request) {
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
+    const user: CreateUserParams = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || '',
+      lastName: last_name || '',
       photo: image_url,
     }
 
@@ -82,9 +82,9 @@ export async function POST(req: Request) {
   if (eventType === 'user.updated') {
     const {id, image_url, first_name, last_name, username } = evt.data
 
-    const user = {
-      firstName: first_name,
-      lastName: last_name,
+    const user: UpdateUserParams = {
+      firstName: first_name || '',
+      lastName: last_name||'',
       username: username!,
       photo: image_url,
     }
